cadbe981e0f187d60eefd0a43f7c93d54fa3c87c
Anthony_Teng <Anthony_Teng@asus.com>
2021-03-24 11:01:32 +0800

    change to use protocol
    Change-Id: I8df9091388caec1444a407b37934d5c9b7c140d2

--- MyPkg/Application/AhciReadWrite/AhciReadWrite.c
+++ MyPkg/Application/AhciReadWrite/AhciReadWrite.c
@@ -6,36 +6,87 @@
  */
 
 #include "AhciReadWrite.h"
-///
-/// The default PCH PCI bus number
-///
-#define DEFAULT_PCI_SEGMENT_NUMBER_PCH 0
-#define DEFAULT_PCI_BUS_NUMBER_PCH 0
-//
-//  SATA Controller Registers (D23:F0)
-//
-#define PCI_DEVICE_NUMBER_PCH_SATA 23
-#define PCI_FUNCTION_NUMBER_PCH_SATA 0
-//
-//  SATA Controller common Registers
-//
-#define R_SATA_CFG_AHCI_BAR                 0x24
+UINT32
+EFIAPI
+AhciReadReg(
+    IN EFI_PCI_IO_PROTOCOL *PciIo,
+    IN UINT32 Offset)
+{
+  UINT32 Data;
+  EFI_STATUS Status;
+
+  ASSERT(PciIo != NULL);
+
+  Data = 0;
+
+  Status = PciIo->Mem.Read(
+      PciIo,
+      EfiPciIoWidthUint32,
+      EFI_AHCI_BAR_INDEX,
+      (UINT64)Offset,
+      1,
+      &Data);
+  if (EFI_ERROR(Status))
+  {
+    Print(L"Error Mem.Read Status -> [%x]\n", Status);
+  }
+  Print(L"Data %x\n", Data);
+  return Data;
+}
+
 EFI_STATUS UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable)
 {
-  UINT32 AhciBar;
-  UINTN PciSataRegBase;
+  EFI_STATUS Status;
+  UINTN TotalDevice;
+  EFI_HANDLE *PciIoHandleBuffer;
+  EFI_PCI_IO_PROTOCOL *PciIo;
 
-  PciSataRegBase = PCI_SEGMENT_LIB_ADDRESS(
-      DEFAULT_PCI_SEGMENT_NUMBER_PCH,
-      DEFAULT_PCI_BUS_NUMBER_PCH,
-      PCI_DEVICE_NUMBER_PCH_SATA,
-      PCI_FUNCTION_NUMBER_PCH_SATA,
-      0
-      );
-  Print(L"PciSataRegBase %x\n", PciSataRegBase);
+  // UINTN SegmentNumber;
+  // UINTN BusNumber;
+  // UINTN DeviceNumber;
+  // UINTN FunctionNumber;
 
-  AhciBar = PciSegmentRead32(PciSataRegBase + R_SATA_CFG_AHCI_BAR) & 0xFFFFF800;
+  PCI_TYPE00 PciData;
+  UINT8 ClassCode;
+  
+  Status = gBS->LocateHandleBuffer(ByProtocol, &gEfiPciIoProtocolGuid, NULL, &TotalDevice, &PciIoHandleBuffer);
+  // Print(L"Number of PCI : %d\n", TotalDevice);
 
+  for (UINTN index = 0; index < TotalDevice; index++)
+  {
+    PciIo = NULL;
+    Status = gBS->HandleProtocol(PciIoHandleBuffer[index], &gEfiPciIoProtocolGuid, &PciIo);
+    if (EFI_ERROR(Status))
+    {
+      Print(L"Error gBS->HandleProtocol Status -> [%x]\n", Status);
+    }
+    // Status = PciIo->GetLocation(PciIo, &SegmentNumber, &BusNumber, &DeviceNumber, &FunctionNumber);
+    // if (EFI_ERROR(Status))
+    // {
+    //   Print(L"Error PciIo->GetLocation Status -> [%x]\n", Status);
+    // }
+    // Print(L"SegmentNumber %x, BusNumber %x, DeviceNumber %x, FunctionNumber %x\n",
+    //       SegmentNumber, BusNumber, DeviceNumber, FunctionNumber);
+
+    Status = PciIo->Pci.Read(PciIo,
+                             EfiPciIoWidthUint8,
+                             PCI_CLASSCODE_OFFSET,
+                             sizeof(PciData.Hdr.ClassCode),
+                             PciData.Hdr.ClassCode);
+
+    ClassCode = PciData.Hdr.ClassCode[1];
+    if (ClassCode == PCI_CLASS_MASS_STORAGE_SATADPA)
+    {
+      break;
+    }
+  }
+
+  if (PciIoHandleBuffer != NULL)
+  {
+    Status = gBS->FreePool(PciIoHandleBuffer);
+  }
+
+  AhciBar = AhciReadReg(PciIo, 0);
   Print(L"AhciBar %x\n", AhciBar);
 
   return 0;
